428ff1f1a13e96b9e4698c65b25fa22109a67a70
anastasiya-zhyrkevich <anastasiya.zhyrkevich@yandex.ru>
2020-05-04 00:30:07 +0300

    first commit [does not work]

---
+++ src/index.web.js
@@ -0,0 +1,15 @@
+
+import * as RN from 'react-native';
+// import * as reactNavigationNative from '@react-navigation/native';
+// import * as reactNavigationStack from '@react-navigation/stack';
+
+import * as RNS from 'react-navigation-stack';
+import * as RNav from '@react-navigation/web';
+
+
+window.RN = RN;
+// window.reactNavigationNative = reactNavigationNative
+// window.reactNavigationStack = reactNavigationStack
+
+window.RNS = RNS;
+window.RNav = RNav;
